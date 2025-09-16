b491ef941bae9dad499c61669421b2e2ed4c1602
paulb <paulb0921@gmail.com>
2025-09-16 16:16:14 -0500

    Update nfl_stat_predictor.py

--- nfl_stat_predictor.py
+++ nfl_stat_predictor.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import joblib
 
